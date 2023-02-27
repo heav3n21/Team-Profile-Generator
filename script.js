f77285b471dde9235c0f87c3830f36aafe011bfc
Matthew <unicomatthew@gmail.com>
2023-02-26 22:02:56 -0800

    added logic for prompt still working on it tho just a progress point

--- script.js
+++ script.js
@@ -1,2 +1,152 @@
-const fs = require('fs')
+
 const inquirer = require('inquirer')
+const fs = require('./functions/function')
+
+
+
+
+
+let questions = function() {
+    inquirer.prompt([
+        {
+            name: 'managerName',
+            type: 'input',
+            message: 'what is the manager`s name?',
+        },
+        {
+            name: 'managerID',
+            type: 'input',
+            message: 'What is your managers ID? ',
+
+        },
+        {
+            name: 'managerEmail',
+            type: 'input',
+            message: 'what is your managers email address?',
+
+        },
+        {
+            name: 'managerOfficeNum',
+            type: 'input',
+            message: 'What is your manager`s office number?',
+        },
+        {
+            name: 'confirm',
+            type: 'list',
+            message: 'Would you like to add a egineer or an itern or would you like to finish building your team?',
+            choices: [
+                'egineer',
+                'intern',
+                'complete build',
+            ]
+        },
+
+
+    ])
+
+        .then(function data(data) {
+            console.log(data)
+            if (data.confirm == `egineer`) {
+                inquirer
+                    .prompt([
+                        {
+                            type: `input`,
+                            name: `engineerName`,
+                            message: `what is the enginner's name? `
+                        },
+                        {
+                            type: `input`,
+                            name: `engineerID`,
+                            message: `what is the enginner's ID? `
+                        },
+                        {
+                            type: `input`,
+                            name: `engineerEmail`,
+                            message: `what is the enginner's email? `
+                        },
+                        {
+                            type: `input`,
+                            name: `engineerGithub`,
+                            message: `what is the enginner's Github username? `
+                        },
+                        {
+                            name: 'confirm',
+                            type: 'list',
+                            message: 'Would you like to add a egineer or an itern or would you like to finish building your team?',
+                            choices: [
+                                'egineer',
+                                'intern',
+                                'all done.',
+                            ]
+                        },
+
+
+
+                    ])
+                   
+               
+                    .then(data => {
+
+                    console.log(data);
+
+                })
+            }
+            else if (data.confirm = 'all done.') {
+                
+
+
+            }
+            else if (data.confirm == `intern`) {
+                inquirer
+                    .prompt([
+                        {
+
+                            type: `input`,
+                            name: `internName`,
+                            message: `what is the intern's name? `
+                        },
+                        {
+                            type: `input`,
+                            name: `internID`,
+                            message: `what is the intern's ID? `
+                        },
+                        {
+                            type: `input`,
+                            name: `internEmail`,
+                            message: `what is the intern's email? `
+                        },
+                        {
+                            type: `input`,
+                            name: `engineerschool`,
+                            message: `what is the enginner's Github username? `
+                        }
+
+
+                    ])
+                    .then(data => {
+
+                        console.log(data);
+
+                    })
+            }
+
+        })
+
+};
+
+
+
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, data, (err) =>
+//         err ? console.log(err) : console.log('works')
+//     );
+
+// }
+
+
+function init() {
+    questions()
+}
+
+
+init();
